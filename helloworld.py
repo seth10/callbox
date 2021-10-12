77983c94f5a9de2ed5aec4ef2f909aa7eab287ac
Seth T <seth10@users.noreply.github.com>
2021-10-11 18:18:41 -0700

    Use Twilio's library to form the response
    Same number of lines, but much simpler to read!

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
+from twilio.twiml.voice_response import VoiceResponse
+
 def lambda_handler(event, context):
-    response = """<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Say voice="alice">hello world!</Say>
-</Response>
-"""
-    response_no_whitespace = ''.join([s.strip() for s in response.split("\n")])
-    return {'statusCode': 200, 'body' : response_no_whitespace}
+    resp = VoiceResponse()
+
+    resp.say('hello world!', voice='alice')
+
+    return {'statusCode': 200, 'body' : str(resp)}
